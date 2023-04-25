964f6b56f12263c7c0b95f2cd967299d2940102c
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-04-24 23:51:12 -0700

    The printer of printf

---
+++ printer.c
@@ -0,0 +1,73 @@
+#include "main.h"
+/**
+ * printf_fto - Print the addresses of characters
+ * @start: The start
+ * @stop: The stop
+ * @beside: the except
+ *
+ * Return: 0
+ */
+int printf_fto(char *start, char *stop, char *beside)
+{
+	int i = 0;
+
+	while (start <= stop)
+	{
+		if (start != except)
+			i += _putchar(*start);
+		start++;
+	}
+	return (i);
+}
+/**
+ * printf_rev - The reverse of strings
+ * @args: The arguments
+ * @par: The structure's parameters
+ *
+ * Return: 0;
+ */
+int printf_rev(va_list args, par_t *par)
+{
+	int len, i = 0;
+	(void)par;
+	char *string = va_arg(args, char *);
+
+	if (string)
+	{
+		for (len = 0; *string; string++)
+			len++;
+		string--;
+		for (; len > 0; len--; string--)
+			i += _putchar(*string);
+	}
+	return (i);
+}
+/**
+ * printf_rot13 - Print rot13
+ * @args: The arguments
+ * @par: The strusture's parameters
+ *
+ * Return: 0
+ */
+int printf_rot13(va_list args, par_t *par)
+{
+	int i = 0, j = 0, count = 0;
+	char array[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLM	nopqrstuvwxyzabcdefghijklm";
+	char *a = va_arg(args, char *);
+	(void)par;
+
+	while (a[i])
+	{
+		if ((a[i] >= 'A' && a[i] <= 'Z')
+				|| (a[i] >= 'a' && a[i] <= 'z'))
+		{
+			j = a[i] - 65;
+			count += _putchar(array[j]);
+		}
+		else
+			count += _putchar(a[i]);
+		i++;
+	}
+	return (count);
+}
